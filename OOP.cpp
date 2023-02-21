6426510a816e752b86ab613fd3ee19a405d46bef
StepanLush <92359271+StepanLush@users.noreply.github.com>
2023-02-21 23:16:50 +0300

    Add files via upload
    Создание иерархии классов

---
+++ OOP.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Book {
+    Author author;
+    string name, genre, place;
+};
+
+class Person {
+public:
+    string name, email;
+    int age;
+};
+
+class User : public Person {
+    bool isDept;
+    vector<Book> bookList;
+};
+
+class Author : public Person{
+    bool isAlive;
+};
+
+class Worker : public Person {
+    int salary;
+    string place;
+};
+
+class Security : public Worker {
+    int shift;
+};
+
+int main()
+{
+
+}
+
